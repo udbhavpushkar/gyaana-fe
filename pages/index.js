17ac8a3c98e9c32ab9ae56a758fcd3eee5857143
udbhavpushkar <udbhavpushkar@gmail.com>
2022-08-03 16:54:48 +0530

    develop inititialise

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Welcome to Gyaana
+          Welcome to Gyaana Develop
         </h1>
         </main>
     </div>
