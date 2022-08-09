511a4951ff06d7d0ab9f7f00043c8795325367f6
sachinsrivastava17 <92143412+sachinsrivastava17@users.noreply.github.com>
2022-08-10 01:26:04 +0530

    Sachin added

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const Header = (props)=>{
-    return <div>
-        Header with logo and title
+const Header = (props) => {
+  return (
+    <div>
+      Header with logo and title
+      <button className="btn btn-primary">Click</button>
     </div>
-}
+  );
+};
 
-export default Header
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
-import React from "react"
-import Layout from "../components/Layout"
+import React from "react";
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
     <Layout>
       This is main page
+      <p>New Changes</p>
     </Layout>
-  )
+  );
 }
