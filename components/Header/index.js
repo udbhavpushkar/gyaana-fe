b5d90dd5d99e581e3d2ed70f8343666d8661f6c0
udbhavpushkar <udbhavpushkar@gmail.com>
2022-08-10 01:05:40 +0530

    layout setup

---
+++ components/Header/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = (props)=>{
+    return <div>
+        Header with logo and title
+    </div>
+}
+
+export default Header
---
+++ components/Layout/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import React from "react";
+import Header from "../Header";
+import Navbar from "../Navbar";
+
+const Layout = (props)=>{
+    return <div>
+        <Head>
+            <title>Gyaana </title>
+        </Head>
+        <Header/>
+        <Navbar/>
+        <div>{props.children}</div>
+    </div>
+}
+
+export default Layout
