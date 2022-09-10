0dc79025f653c82e4c8be5975fb8e20e177705e8
udbhavpushkar <udbhavpushkar@gmail.com>
2022-09-10 23:28:45 +0530

    logout added

--- pages/Admin/index.js
+++ pages/Admin/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { isLoggedIn } from "../../utilities/auth_services"
+import { isLoggedIn, logout } from "../../utilities/auth_services"
 import { getRequest } from "../../utilities/rest_service"
 import styles from "./styles.module.css"
 
@@ -19,6 +19,7 @@ const Admin = () => {
         <div className={isToggle ? styles.adminLeftContanier : styles.adminLeftContanierClose}>
           Left Navdation
           <p onClick={() => setIsToggle(!isToggle)}>X</p>
+          <div onClick={logout}>Logout</div>
         </div>
         <div className={styles.adminRightContanier}>Right Content</div>
       </div>
