ea114449e8e7e80e89c57b6fa18078bab55830f9
sachinsrivastava17 <92143412+sachinsrivastava17@users.noreply.github.com>
2022-09-28 17:04:53 +0530

    added enquiry form

--- components/Enquiry/index.js
+++ components/Enquiry/index.js
@@ -1,49 +1,49 @@
 import React from "react"
 import styles from "./styles.module.css"
 import Image from "next/image"
-import Logo from "../../assets/images/loginImg.jpg"
+import Logo from "../../assets/images/unnamed.jpg"
 
 const Enquiry = () => {
 	return (
-		<div className="row">
+		<div className="row" style={{ backgroundColor: "rgb(127, 81, 127)", color: "white" }}>
 			<div className="col-md-6 p-4 text-center">
-				<Image src={Logo} height={500} width={400} />
+				<Image src={Logo} height={540} width={500} />
 			</div>
 			<div className="col-md-6 p-4">
 				<h2 className="styles.heading">Enquiry Form</h2>
 				<form>
-					<div class="mb-3">
-						<label for="name" class="form-label">
+					<div className="mb-3">
+						<label for="name" className="form-label">
 							Full Name
 						</label>
-						<input type="text" class="form-control" id="name" aria-describedby="emailHelp" />
+						<input type="text" className="form-control" id="name" aria-describedby="emailHelp" />
 					</div>
-					<div class="mb-3">
-						<label for="email" class="form-label">
+					<div className="mb-3">
+						<label for="email" className="form-label">
 							Email address
 						</label>
-						<input type="email" class="form-control" id="email" aria-describedby="emailHelp" />
+						<input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
 					</div>
-					<div class="mb-3">
-						<label for="contact" class="form-label">
+					<div className="mb-3">
+						<label for="contact" className="form-label">
 							Contant
 						</label>
-						<input type="text" class="form-control" id="contact" aria-describedby="emailHelp" />
+						<input type="text" className="form-control" id="contact" aria-describedby="emailHelp" />
 					</div>
-					<div class="mb-3">
-						<label for="address" class="form-label">
+					<div className="mb-3">
+						<label for="address" className="form-label">
 							Address
 						</label>
-						<input type="text" class="form-control" id="address" aria-describedby="emailHelp" />
+						<input type="text" className="form-control" id="address" aria-describedby="emailHelp" />
 					</div>
-					<div class="mb-3">
-						<label for="message" class="form-label">
+					<div className="mb-3">
+						<label for="message" className="form-label">
 							Message
 						</label>
-						<textarea type="text" class="form-control" id="message" aria-describedby="emailHelp" />
+						<textarea type="text" className="form-control" id="message" aria-describedby="emailHelp" />
 					</div>
 
-					<button type="submit" class="btn btn-primary">
+					<button type="submit" className="btn btn-success">
 						Submit
 					</button>
 				</form>
