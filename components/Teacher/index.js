1d2c0a151a663067b61595d5970d0cff2951ddd6
sachinsrivastava17 <92143412+sachinsrivastava17@users.noreply.github.com>
2022-11-21 00:51:41 +0530

    added timetable for teacher and fixed sidebar

--- components/Teacher/index.js
+++ components/Teacher/index.js
@@ -4,71 +4,71 @@ import { isLoggedIn, logout } from "../../utilities/auth_services"
 import styles from "./style.module.css"
 import imageLogo from "../../assets/images/unnamed.jpg"
 import Link from "next/link"
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router"
 
 const TeacherLayout = (props) => {
-    const router = useRouter()
-    const [isToggle, setIsToggle] = useState(true)
+	const router = useRouter()
+	const [isToggle, setIsToggle] = useState(true)
 
-    const headersData = [
-        {
-            name: "Home",
-            link: "/teacher",
-        },
-        {
-            name: "Time Table",
-            link: "/teacher/timetable",
-        },
-        {
-            name: "Notice",
-            link: "/teacher/notice",
-        },
-    ]
+	const headersData = [
+		{
+			name: "Home",
+			link: "/teacher",
+		},
+		{
+			name: "Time Table",
+			link: "/teacher/timetable",
+		},
+		{
+			name: "Notice",
+			link: "/teacher/notice",
+		},
+	]
 
-    useEffect(() => {
-        if (!isLoggedIn() || localStorage.getItem("role") !== "teacher") {
-            router.push("/")
-        }
-    }, [])
+	useEffect(() => {
+		if (!isLoggedIn() || localStorage.getItem("role") !== "teacher") {
+			router.push("/")
+		}
+	}, [])
 
-    const handleToggle = () => {
-        setIsToggle(!isToggle)
-    }
+	const handleToggle = () => {
+		setIsToggle(!isToggle)
+	}
 
-    return (
-        <div className={styles.adminWrapper}>
-            <div className={styles.adminContanier}>
-                <div className={isToggle ? styles.adminLeftContanier : styles.sidebarClose}>
-                    <p className={styles.closeButton} onClick={() => setIsToggle(!isToggle)}>
-                        <span style={{ fontSize: "20px" }}>X</span>
-                    </p>
-                    <div>
-                        <div style={{ display: "flex", justifyContent: "center" }}>
-                            <Image src={imageLogo} height={100} width={200} />
-                        </div>
-                        <div className={styles.linkContainer} style={{ textAlign: "center", marginTop: "20px" }}>
-                            {headersData.map((item, index) => (
-                                <Link href={item.link} key={`nav_${index}`}>
-                                    <p className={styles.adminHeaderLinks}>{item.name}</p>
-                                </Link>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-                <div className={styles.adminRightContanier}>
-                    <div className={styles.menuButtonContainer}>
-                        <button onClick={handleToggle} className={isToggle ? styles.sidebarClose : styles.sidebarOpen}>
-                            &#9776;
-                        </button>
-                        <button className={styles.logoutBtn} onClick={logout}>
-                            Logout
-                        </button>
-                    </div>
-                    <div>{props.children}</div>
-                </div>
-            </div>q
-        </div>
-    )
+	return (
+		<div className={styles.adminWrapper}>
+			<div className={styles.adminContanier}>
+				<div className={isToggle ? styles.adminLeftContanier : styles.sidebarClose}>
+					<p className={styles.closeButton} onClick={() => setIsToggle(!isToggle)}>
+						<span style={{ fontSize: "20px" }}>X</span>
+					</p>
+					<div>
+						<div style={{ display: "flex", justifyContent: "center" }}>
+							<Image src={imageLogo} height={100} width={200} />
+						</div>
+						<div className={styles.linkContainer} style={{ textAlign: "center", marginTop: "20px" }}>
+							{headersData.map((item, index) => (
+								<Link href={item.link} key={`nav_${index}`}>
+									<p className={styles.adminHeaderLinks}>{item.name}</p>
+								</Link>
+							))}
+						</div>
+					</div>
+				</div>
+				<div className={styles.adminRightContanier}>
+					<div className={styles.menuButtonContainer}>
+						<button onClick={handleToggle} className={isToggle ? styles.sidebarClose : styles.sidebarOpen}>
+							&#9776;
+						</button>
+						<button className={styles.logoutBtn} onClick={logout}>
+							Logout
+						</button>
+					</div>
+					<div>{props.children}</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default TeacherLayout
