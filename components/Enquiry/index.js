936cfb3a55d50e171139dfe2edf3acbadf663a56
sachinsrivastava17 <92143412+sachinsrivastava17@users.noreply.github.com>
2022-12-10 05:02:09 +0530

    added validation to enquiry form

--- components/Enquiry/index.js
+++ components/Enquiry/index.js
@@ -6,28 +6,55 @@ import { postRequest } from "../../utilities/rest_service"
 import { toast } from "react-toastify"
 
 const Enquiry = () => {
-	const [formData, setFormData] = useState({})
+	const initialValues = { name: "", email: "", mobile: "", address: "", messsage: "" }
+	const [formData, setFormData] = useState(initialValues)
+	const [errorData, setErrorData] = useState({})
 
 	const handleAddFeedback = async (e) => {
 		e.preventDefault()
-		try {
-			let response = await postRequest("feedback/", formData)
-			if (response.isSuccess) {
-				toast.success("Enqiry Submitted Successfully")
-				document.getElementById("add-feedback-form").reset()
-				setFormData({})
-			} else {
-				toast.error("Request Failed")
+		setErrorData(validate(formData))
+		if (Object.keys(errorData).length !== 0) {
+			try {
+				let response = await postRequest("feedback/", formData)
+				if (response.isSuccess) {
+					toast.success("Enqiry Submitted Successfully")
+					document.getElementById("add-feedback-form").reset()
+					setFormData({})
+				} else {
+					toast.error("Request Failed")
+				}
+			} catch (e) {
+				console.log("Error: ", e)
 			}
-		} catch (e) {
-			console.log("Error: ", e)
 		}
 	}
 
 	const handleFormChange = (e) => {
-		let data = { ...formData }
-		data[e.target.name] = e.target.value
-		setFormData(data)
+		const { name, value } = e.target
+		setFormData({ ...formData, [name]: value })
+		// let data = { ...formData }
+		// data[e.target.name] = e.target.value
+		// setFormData(data)
+	}
+
+	const validate = (val) => {
+		const errors = {}
+		if (!val.name) {
+			errors.name = "Name is Required"
+		}
+		if (!val.email) {
+			errors.email = "Email is Required"
+		}
+		if (!val.mobile) {
+			errors.mobile = "Contact is Required"
+		}
+		if (!val.address) {
+			errors.address = "Address is Address"
+		}
+		if (!val.message) {
+			errors.message = "Message is Required"
+		}
+		return errors
 	}
 
 	return (
@@ -43,30 +70,35 @@ const Enquiry = () => {
 							Full Name
 						</label>
 						<input type="text" className="form-control" id="name" name="name" onChange={handleFormChange} aria-describedby="emailHelp" />
+						<p style={{ color: "red" }}>{errorData.name}</p>
 					</div>
 					<div className="mb-3">
 						<label htmlFor="email" className="form-label">
 							Email address
 						</label>
 						<input type="email" className="form-control" id="email" name="email" onChange={handleFormChange} aria-describedby="emailHelp" />
+						<p style={{ color: "red" }}>{errorData.email}</p>
 					</div>
 					<div className="mb-3">
 						<label htmlFor="mobile" className="form-label">
 							Contant
 						</label>
 						<input type="text" className="form-control" id="mobile" name="mobile" onChange={handleFormChange} aria-describedby="emailHelp" />
+						<p style={{ color: "red" }}>{errorData.mobile}</p>
 					</div>
 					<div className="mb-3">
 						<label htmlFor="address" className="form-label">
 							Address
 						</label>
 						<input type="text" className="form-control" id="address" name="address" onChange={handleFormChange} aria-describedby="emailHelp" />
+						<p style={{ color: "red" }}>{errorData.address}</p>
 					</div>
 					<div className="mb-3">
 						<label htmlFor="message" className="form-label">
 							Message
 						</label>
 						<textarea type="text" className="form-control" id="message" name="message" onChange={handleFormChange} aria-describedby="emailHelp" />
+						<p style={{ color: "red" }}>{errorData.message}</p>
 					</div>
 
 					<button type="submit" className="btn btn-success">
--- pages/Contact/index.js
+++ pages/Contact/index.js
@@ -3,69 +3,71 @@ import Layout from "./../../components/Layout/index"
 import styles from "./contact.module.css"
 import Image from "next/image"
 import LoadingSpinner from "./../../components/LoadingSpinner/index"
+import Enquiry from "./../../components/Enquiry/index"
 
 const Contact = () => {
-  const [isLoading, setIsLoading] = useState(true)
+	const [isLoading, setIsLoading] = useState(true)
 
-  const onclick = (item) => {
-    console.log(item)
-  }
-  const newsData = [
-    {
-      heading: "Address",
-      desc: "Gyana Internaational School, Faizabad (UP)",
-    },
-    {
-      heading: "Contact",
-      desc: "+91 889900119988",
-    },
-    {
-      heading: "Email",
-      desc: "gyaana@gmail.com",
-    },
-    {
-      heading: "Website",
-      desc: "www.gyaana.co",
-    },
-  ]
+	const onclick = (item) => {
+		console.log(item)
+	}
+	const newsData = [
+		{
+			heading: "Address",
+			desc: "Gyana Internaational School, Faizabad (UP)",
+		},
+		{
+			heading: "Contact",
+			desc: "+91 889900119988",
+		},
+		{
+			heading: "Email",
+			desc: "gyaana@gmail.com",
+		},
+		{
+			heading: "Website",
+			desc: "www.gyaana.co",
+		},
+	]
 
-  useEffect(() => {
-    setIsLoading(true)
-    setTimeout(() => {
-      setIsLoading(false)
-    }, 700)
-  }, [])
-  return (
-    <Layout>
-      {isLoading ? (
-        <LoadingSpinner />
-      ) : (
-        <div className={styles.newsWrapper}>
-          <div className={styles.newsHeading}>
-            <p>Contact</p>
-          </div>
-          <div className="row">
-            <div>
-              <ul className="list-group">
-                {newsData.map((item) => (
-                  <li className={`list-group-item ${styles.listContainer}`} style={{ borderBottom: "2px dotted rgb(170, 224, 123)" }}>
-                    <div className={`row ${styles.newsContentContainer}`}>
-                      <div className="col-md-8 ">
-                        <h4 onClick={() => onclick(item)} className={`center ${styles.newsHeadingText}`}>
-                          {item.heading}
-                        </h4>
-                        <p style={{ color: "black" }}>{item.desc}</p>
-                      </div>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        </div>
-      )}
-    </Layout>
-  )
+	useEffect(() => {
+		setIsLoading(true)
+		setTimeout(() => {
+			setIsLoading(false)
+		}, 700)
+	}, [])
+	return (
+		<Layout>
+			{isLoading ? (
+				<LoadingSpinner />
+			) : (
+				<div className={styles.newsWrapper}>
+					<div className={styles.newsHeading}>
+						<p>Contact</p>
+					</div>
+					<div className="row">
+						<div>
+							<ul className="list-group">
+								{newsData.map((item) => (
+									<li className={`list-group-item ${styles.listContainer}`} style={{ borderBottom: "2px dotted rgb(170, 224, 123)" }}>
+										<div className={`row ${styles.newsContentContainer}`}>
+											<div className="col-md-8 ">
+												<h4 onClick={() => onclick(item)} className={`center ${styles.newsHeadingText}`}>
+													{item.heading}
+												</h4>
+												<p style={{ color: "black" }}>{item.desc}</p>
+											</div>
+										</div>
+									</li>
+								))}
+							</ul>
+						</div>
+					</div>
+					<Enquiry />
+				</div>
+			)}
+		</Layout>
+	)
 }
 
 export default Contact
