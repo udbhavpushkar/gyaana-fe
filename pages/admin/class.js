172e87c470372223b436ad77c4923402e1d06928
udbhavpushkar <udbhavpushkar@gmail.com>
2023-01-28 17:38:36 +0530

    deleted unused files

--- pages/admin/class.js
+++ pages/admin/class.js
@@ -1,6 +1,5 @@
 import React from "react"
 import Grade from "../../components/Admin/class"
-import CreateClass from "../../components/Admin/class/createClass"
 import AdminLayout from "../../components/Admin/index"
 
 const AdminClassList = (props) => {
