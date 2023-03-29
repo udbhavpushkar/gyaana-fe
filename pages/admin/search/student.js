71f2392974789987af1abd2b03245f8bece125f7
udbhavpushkar <udbhavpushkar@gmail.com>
2023-03-29 23:56:27 +0530

    student list and filters

--- pages/admin/search/student.js
+++ pages/admin/search/student.js
@@ -1,147 +1,119 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import AdminLayout from "../../../components/Admin"
+import Collapsable from "../../../components/Custom/Collapsable"
+import { getRequest, patchRequest } from "../../../utilities/rest_service"
 import style from "./styles.module.css"
-
-const TABLE_HEADERS = ["Name", "Class", "Section", "Roll No", "Contact", "City"]
-
-const TABLE_DATA = [
-	{
-		name: "Sachin Srivastava",
-		class: "IX",
-		section: "A",
-		roll_no: "12",
-		city: "Faizabad",
-		contact: "0000011111",
-	},
-	{
-		name: "xyz",
-		class: "xyz",
-		section: "xyz",
-		roll_no: "xyz",
-		city: "xyz",
-		contact: "xyz",
-	},
-	{
-		name: "xyz",
-		class: "xyz",
-		section: "xyz",
-		roll_no: "xyz",
-		city: "xyz",
-		contact: "xyz",
-	},
-	{
-		name: "xyz",
-		class: "xyz",
-		section: "xyz",
-		roll_no: "xyz",
-		city: "xyz",
-		contact: "xyz",
-	},
-	{
-		name: "xyz",
-		class: "xyz",
-		section: "xyz",
-		roll_no: "xyz",
-		city: "xyz",
-		contact: "xyz",
-	},
-	{
-		name: "xyz",
-		class: "xyz",
-		section: "xyz",
-		roll_no: "xyz",
-		city: "xyz",
-		contact: "xyz",
-	},
-]
 
 const Student = () => {
 	const [isFilter, setISFiletr] = useState(false)
+	const [searchText, setSearchText] = useState("")
+	const [studentList, setStudentList] = useState([])
+	const [filterStudentList, setFilterStudentList] = useState([])
 
-	const handleFilter = (e) => {
-		e.preventDefault()
-		setISFiletr(true)
+	useEffect(() => {
+		fetchStudent()
+	}, [])
+
+	const fetchStudent = async (e) => {
+		try {
+			let response = await getRequest(`student/`)
+			if (response.isSuccess) {
+				setStudentList(response.data)
+			}
+		} catch (error) {
+			console.log(error);
+		}
 	}
+
+	const handleSearch = (e) => {
+		e.preventDefault()
+		let text = e.target.value
+		setSearchText(text)
+		let data = studentList.filter((d => {
+			let result = d?.admissionNo?.includes(text) || d?.grade?.name?.includes(text) || d?.gender?.includes(text) || d?.userId?.firstName?.includes(text) || d?.userId?.lastName?.includes(text) || d?.userId?.email?.includes(text);
+			return result
+		}))
+		if (text) {
+			setFilterStudentList(data)
+		} else {
+			setFilterStudentList([])
+		}
+	}
+
+	const handleToggleActive = async (e, id, active) => {
+		try {
+			//requires BE changes, wont work for now
+			let response = await patchRequest(`student/${id}`, { disabled: active })
+			if (response.isSuccess) {
+				let data = [...employeeList]
+				let index = data.findIndex(d => d._id == id)
+				if (data[index]) {
+					data[index] = response.data
+					setEmployeeList(data)
+				}
+			}
+			console.log(e.target.value);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
 	return (
 		<AdminLayout>
 			<div style={{ boxShadow: "0px 3px 6px rgba(128, 128, 128, 0.636)" }}>
-				<form style={{ padding: "30px" }}>
-					<div className="text-center mb-3">
-						<h3>Search Student Information</h3>
-						<hr />
-					</div>
-					<div className={` ${style.search_admissionNo}`}>
-						<h5>Search by Admission Number</h5>
-						<input type="text" placeholder="Enter Admission Number" />
-					</div>
-					<h5 className="text-center m-2">OR</h5>
-					<div className={` ${style.search_admissionNo}`}>
-						<h5>Search by Student Name/Class</h5>
-						<div>
-							<label className="m-2 "> Name </label>
-							<input type="text" placeholder="Student Name" />
-						</div>
-						<div>
-							<label className="m-2">Class </label>
-							<input type="text" placeholder="Student Class" />
-						</div>
-						<div>
-							<label className="mx-1 mt-2">Section </label>
-							<input type="text" placeholder="Student Section" />
-						</div>
-					</div>
-					<div className="text-center m-4">
-						<button className="btn btn-success btn-sm" onClick={handleFilter}>
-							Search
-						</button>
-						<button className="btn btn-danger btn-sm mx-4">Clear</button>
-					</div>
-				</form>
-				{/* showing search result in table start   */}
-				{isFilter && (
-					<div style={{ marginLeft: "30px", fontSize: "18px" }}>
-						<span style={{ color: "purple" }}>
-							Filter: Showing <b>10</b> results
-						</span>
-						<span>
-							<button
-								className="btn btn-sm btn-warning mx-2"
-								onClick={() => setISFiletr(false)}
+				<div className="text-center mb-3">
+					<h3>Search Student</h3>
+					<hr />
+				</div>
+				<div>
+					<div>
+						<div className="form-group row">
+							<label
+								className="col-sm-2 col-form-label"
 							>
-								Clear Filter
-							</button>
-						</span>
+								Filter fields
+							</label>
+							<div className="col-sm-8 my-2">
+								<input onChange={handleSearch} type="text" className="form-control" />
+							</div>
+						</div>
+
 					</div>
-				)}
-				<div style={{ padding: "20px" }}>
-					<table class={`table ${style.selectStudent}`}>
+				</div>
+
+				<Collapsable heading="Student List">
+					<table className="table">
 						<thead>
 							<tr>
-								{TABLE_HEADERS.map((heading) => {
-									return (
-										<th key={{ heading }} scope="col">
-											{heading}
-										</th>
-									)
-								})}
+								<th scope="col">#</th>
+								<th scope="col">Admission No</th>
+								<th scope="col">Name</th>
+								<th scope="col">Class</th>
+								<th scope="col">Section</th>
+								<th scope="col">Active/Inactive</th>
 							</tr>
 						</thead>
 						<tbody>
-							{TABLE_DATA.map((item, i) => {
-								return (
-									<tr key={item.name + i}>
-										<td>{item.name}</td>
-										<td>{item.class}</td>
-										<td>{item.section}</td>
-										<td>{item.roll_no}</td>
-										<td>{item.city}</td>
-										<td>{item.contact}</td>
-									</tr>
-								)
+							{(searchText ? filterStudentList : studentList).map((data, index) => {
+								let active = !data?.userId?.disabled
+								let btnText = active ? "Active" : "Inactive"
+								return <tr key={data._id}>
+									<td>{index + 1}</td>
+									<td>{data?.admissionNo}</td>
+									<td>{data?.userId?.firstName} {data?.userId?.lastName}</td>
+									<td>{data?.grade?.name}</td>
+									<td>{data?.section?.name}</td>
+									<td>
+										<button
+											className={`btn btn-outline ${active ? 'btn-outline-success' : 'btn-outline-danger'}`}
+											onClick={(e) => { handleToggleActive(e, data?._id, active) }}
+										>{btnText}</button>
+									</td>
+								</tr>
 							})}
 						</tbody>
 					</table>
-				</div>
+				</Collapsable>
 				{/* showing search result in table end   */}
 			</div>
 		</AdminLayout>
