10cee405ab91bcec927320a5290e84bf56c47883
sachinsrivastava17 <92143412+sachinsrivastava17@users.noreply.github.com>
2023-04-15 01:22:00 +0530

    added student edit

--- utilities/form_services.js
+++ utilities/form_services.js
@@ -1,5 +1,11 @@
 export function handleFormData(e, formData, setFormData) {
-    let data = { ...formData }
-    data[e.target.name] = e.target.value
-    setFormData(data)
+	let data = { ...formData }
+
+	// setting input inside user object
+	if (e.target.name == "firstName" || "lastName") {
+		data["userId"][e.target.name] = e.target.value
+	}
+
+	data[e.target.name] = e.target.value
+	setFormData(data)
 }
